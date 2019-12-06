31bb2a116b472af136a268f65e104fbe2ff375f0
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-12-06 17:15:17 +0530

    getting image path from the backend

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ProductModel} from '../models/productModel';
 import {Observable, Subject} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,28 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getAllProducts() {
-    this.http.get<{message: string, products: any[]}>(this.url)
-      .pipe()
-      .subscribe(result => {
-      this.products = result.products;
+    this.http.get<{message: string, products: {
+        _id: string,
+        name: string,
+        category: string,
+        price: number,
+        imagePath: string,
+        condition: string
+      }[]}>(this.url)
+      .pipe(map(productData => {
+        return productData.products.map(product => {
+          return{
+            id: product._id,
+            name: product.name,
+            price: product.price,
+            category: product.category,
+            imagePath: product.imagePath,
+            condition: product.condition
+          };
+        });
+      }))
+      .subscribe(products => {
+      this.products = products;
       this.productSubject.next([...this.products]);
     });
   }
